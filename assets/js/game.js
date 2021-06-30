ec90340d44d1eb4514a0c9ec6784b35c460d35c8
Joshua Scott <84161503+Bumblesnuff@users.noreply.github.com>
2021-06-30 17:42:23 -0400

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -71,22 +71,54 @@ var fight = function(enemyName) {
     }
 };
 // execute function
-for(var i = 0; i < enemyNames.length; i++) {
+// Function to start a new game
+
+// function to start a new game
+var startGame = function() {
+    // reset player stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+
+    for (var i = 0; i < enemyNames.length; i++) {
+      if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+  
+        var pickedEnemyName = enemyNames[i];
+  
+        enemyHealth = 50;
+  
+        fight(pickedEnemyName);
+      }
+      else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+      }
+      endGame();
+    };
+
+startGame();
+};
+
+var endGame = function() {
     if (playerHealth > 0) {
-        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
     }
-    var pickedEnemyName = enemyNames[i];
+    else {
+        window.alert("You've lost your robot in battle.");
+    }
+    var playAgainConfirm = window.confirm("Would you like to play again?");
 
-    enemyHealth = 50;
-    
-     // use debugger to pause script from running and check what's going on at that moment in the code
-    // debugger;
-
-    fight(pickedEnemyName)
-  }
-
+    if (playAgainConfirm) {
+        startGame();
+    }
+    else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
+};
 // Game States
 // "WIN" - Player robot has defeated all enemy-robots
 //    * Fight all enemy-robots
 //    * Defeat each enemy-robot
 // "LOSE" - Player robot's health is zero or less
+// A functoin declaration pulls the function to the top of the code so you can run it before it is declared!
